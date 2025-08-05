10083c2c58fa8d957ab7ff4385b1f5c725c5ffff
Natan <natanalmeida0401@gmail.com>
2025-08-04 23:31:04 -0300

    feat: criando o dto do projeto e colocando o loombok nele

--- src/main/java/controller/PropostaController.java
+++ src/main/java/controller/PropostaController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import dto.PropostaRequestDto;
+import dto.PropostaResponseDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/proposta")
 public class PropostaController {
-    private PropostaService propostaService;
+  //  private PropostaService propostaService;
 
-/*    @PostMapping
+    @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
         PropostaResponseDto response = propostaService.criar(requestDto);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
@@ -14,6 +23,7 @@ public class PropostaController {
                         .toUri())
                 .body(response);
     }
+/*
 
     @GetMapping
     public ResponseEntity<List<PropostaResponseDto>> obterProposta() {
