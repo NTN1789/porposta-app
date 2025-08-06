7e9bd69c5f08aa3ea5bc13c76494acfebfbd9d47
Natan <natanalmeida0401@gmail.com>
2025-08-06 00:07:04 -0300

    feat: colocando mapper vou configurar amanha

--- src/main/java/service/PropostaService.java
+++ src/main/java/service/PropostaService.java
@@ -1,19 +1,28 @@
 package service;
 
+import com.project.proposta_app.dto.PropostaRequestDto;
+import com.project.proposta_app.dto.PropostaResponseDto;
+import com.project.proposta_app.entity.Proposta;
+import com.project.proposta_app.repository.PropostaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class PropostaService {
 
 
+private PropostaRepository propostaRepository;
 
-  /*  public PropostaResponseDto criar(PropostaRequestDto requestDto) {
+    public PropostaResponseDto criar(PropostaRequestDto requestDto) {
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDto);
         propostaRepository.save(proposta);
 
-        notificarRabbitMQ(proposta);
+   //     notificarRabbitMQ(proposta);
 
-        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+     //   return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+
+    return  null;
     }
-*/
+
 }
